7ea55de6945e7b53b074c260ed0ebddbabcf28a1
Joshua <j2h5068312@gmail.com>
2022-12-29 22:01:27 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose') // 載入 mongoose
 
-app.engine('handlebars', exphbs({defaultLayout:'main'}))
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+
+
+const port = 3000
+app.use(express.urlencoded({ extended: true }))
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting routes
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
   res.render('index')
 })
 
---
+++ models/Shortener.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const shortenerSchema = new Schema({
+  name: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  done: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Shortener', shortenerSchema)
